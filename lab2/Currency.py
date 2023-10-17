d48497ce75e6fd0c329cf67d4f90bcd386f536ac
Ethan Ton <ethan.m.ton@gmail.com>
2023-10-16 17:17:16 -0700

    declared methods in Currency

--- lab2/Currency.py
+++ lab2/Currency.py
@@ -2,3 +2,27 @@ class Currency:
 
     noteValue,coinValue = 0
     
+    #Default Constructor
+    def __init__() -> None:
+        pass
+    def __init__(double):
+        pass
+    
+    #Assignment
+    def __init__(self):
+        pass
+
+    def add():
+        pass
+
+    def subtract():
+        pass
+
+    def isEqual():
+        pass
+
+    def isGreater():
+        pass
+
+    def print():
+        pass
