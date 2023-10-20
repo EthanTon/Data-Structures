52520494972f9924481fba83379e07b8f80455b4
alexander-cho <44910451+alexander-cho@users.noreply.github.com>
2023-10-20 15:22:41 -0700

    Add files via upload

--- lab2/Dollar.py
+++ lab2/Dollar.py
@@ -8,6 +8,36 @@ class Dollar(Currency):
     #additional name attribute
     def name(self):
         return self.currency_name
+            
+    def isSameCurrency(self, comparand):
+        if self.currency_name == comparand.currency_name:
+            return True
+        else:
+            return False
+    
+    def add(self, addend):
+        if self.isSameCurrency(addend):
+            super().add(addend)
+        else:
+            print("Invalid addition")
+    
+    def subtract(self, subtrahend):
+        if self.isSameCurrency(subtrahend):
+            super().subtract(subtrahend)
+        else:    
+            print("Invalid subtraction")
+        
+    def isEqual(self, comparison):
+        if self.isSameCurrency(comparison):
+            super().isEqual(comparison)
+        else:    
+            print("Invalid comparison")
+        
+    def isGreater(self, compare):
+        if self.isSameCurrency(compare):
+            super().isGreater(compare)
+        else:    
+            print("Invalid comparison")
     
     def print(self):
         super().print()
--- lab2/Pound.py
+++ lab2/Pound.py
@@ -9,8 +9,36 @@ class Pound(Currency):
     def name(self):
         return self.currency_name
     
+    def isSameCurrency(self, comparand):
+        if self.currency_name == comparand.currency_name:
+            return True
+        else:
+            return False
+    
+    def add(self, addend):
+        if self.isSameCurrency(addend):
+            super().add(addend)
+        else:
+            print("Invalid addition")
+    
+    def subtract(self, subtrahend):
+        if self.isSameCurrency(subtrahend):
+            super().subtract(subtrahend)
+        else:   
+            print("Invalid subtraction")
+        
+    def isEqual(self, comparison):
+        if self.isSameCurrency(comparison):
+            super().isEqual(comparison)
+        else:    
+            print("Invalid comparison")
+        
+    def isGreater(self, compare):
+        if self.isSameCurrency(compare):
+            super().isGreater(compare)
+        else:
+            print("Invalid comparison")
+    
     def print(self):
         super().print()
         print(self.name(), end = " ")
-
-    
