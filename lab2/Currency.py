e7cc684d266ad1faf1059118a914f3ceb2a618e0
Ethan Ton <ethan.m.ton@gmail.com>
2023-10-23 16:31:24 -0700

    added virtual getname()

--- lab2/Currency.py
+++ lab2/Currency.py
@@ -14,18 +14,25 @@ class Currency:
             (print("Invalid value"))
             raise ValueError
 
-
     def add(self,addend):
-        self.noteValue += addend.noteValue
-        coinValueTemp = self.coinValue + addend.coinValue
-        if(coinValueTemp > 100): 
-            self.noteValue = self.noteValue + 1
-            self.coinValue = coinValueTemp - 100
-        else:  self.coinValue = coinValueTemp
+        try:
+            if(addend.getname()!=self.getname()): raise ValueError
+            self.noteValue += addend.noteValue
+            coinValueTemp = self.coinValue + addend.coinValue
+            if(coinValueTemp > 100): 
+                self.noteValue = self.noteValue + 1
+                self.coinValue = coinValueTemp - 100
+            else:  self.coinValue = coinValueTemp
+        except ValueError: ("Invalid addition")
+
+        
+
+    def getname(): print("in child class")
 
     
     def subtract(self,subtrahend):
         try:
+            if(subtrahend.getname()!=self.getname()): raise ValueError
             self.noteValue = self.noteValue - subtrahend.noteValue
 
             coinTempValue = self.coinValue - subtrahend.coinValue
@@ -44,14 +51,23 @@ class Currency:
             
         
     def isEqual(self,comparand) -> bool:
-        if comparand.noteValue == self.noteValue and comparand.coinValue == self.coinValue: return True
+        try:
+            if(comparand.getname()!=self.getname()): raise ValueError
+            if comparand.noteValue == self.noteValue and comparand.coinValue == self.coinValue: return True
+        except ValueError: 
+            print("Invalid operation")
         return False
 
     def isGreater(self,comparand) -> bool:
-        if comparand.noteValue > self.noteValue: return False
-        if comparand.coinValue == self.noteValue:
-            if comparand.coinValue > self.coinValue: return False
-        return True
+        try:
+            if(comparand.getname()!=self.getname()): raise ValueError
+            if comparand.noteValue > self.noteValue: return False
+            if comparand.noteValue == self.noteValue:
+                if comparand.coinValue > self.coinValue: return False
+            return True
+        except ValueError: 
+            print("Invalid operation")
+            return False
 
     def print(self):
         print("%.2f" % (self.noteValue + (self.coinValue/100)), end = " ")
