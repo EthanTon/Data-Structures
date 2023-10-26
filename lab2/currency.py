e5f1263cd93ff037e764a99960174e33569d38dc
Ethan Ton <ethan.m.ton@gmail.com>
2023-10-25 20:08:41 -0700

    Update currency.py

--- lab2/currency.py
+++ lab2/currency.py
@@ -35,14 +35,17 @@ class Currency:
     def add(self,addend):     
     
         try:
-            if(addend.getName()!=self.getName()): raise ValueError
+            if(addend.getName()!=self.getName()):
+                raise ValueError
             self.noteValue += addend.noteValue
             coinValueTemp = self.coinValue + addend.coinValue
             if(coinValueTemp > 100): 
                 self.noteValue = self.noteValue + 1
                 self.coinValue = coinValueTemp - 100
-            else:  self.coinValue = coinValueTemp
-        except ValueError: ("Invalid addition")
+            else:
+                self.coinValue = coinValueTemp
+        except ValueError:
+            print("Invalid addition")
 
     def subtract(self,subtrahend):   
         """
@@ -54,22 +57,25 @@ class Currency:
         Returns: None
         """
         try:
-            if(subtrahend.getName()!=self.getName()): raise ValueError("InvalidSubtrahend")
+            if subtrahend.getName() != self.getName():
+                raise ValueError("InvalidSubtrahend")
             self.noteValue = self.noteValue - subtrahend.noteValue
 
             coinTempValue = self.coinValue - subtrahend.coinValue
 
-            if(coinTempValue < 0): 
+            if coinTempValue < 0:
                 self.noteValue = self.noteValue - 1
                 self.coinValue = 100+coinTempValue
                 if self.noteValue < 0: 
                     raise ValueError
-            else: self.coinValue = coinTempValue
-            if self.noteValue < 0 or self.coinValue < 0: raise ValueError
-        except ValueError: 
+            else:
+                self.coinValue = coinTempValue
+            if self.noteValue < 0 or self.coinValue < 0:
+                raise ValueError
+        except ValueError:
             print("Invalid subtraction")
-            self.add(subtrahend)
-        except ValueError("InvalidSubtrahend"): print("Invalid subtraction")
+            if self.noteValue < 0:
+                self.add(subtrahend)
 
     def isEqual(self,comparand) -> bool:
         """
@@ -82,7 +88,7 @@ class Currency:
             bool: True if the noteValue and coinValue of both objects are equal, otherwise False.
         """
         try:
-            if(comparand.getName()!=self.getName()): raise ValueError
+            if comparand.getName() != self.getName(): raise ValueError
             if comparand.noteValue == self.noteValue and comparand.coinValue == self.coinValue: return True
         except ValueError: 
             print("Invalid operation")
@@ -98,7 +104,8 @@ class Currency:
         Return: True if the current currency object is greater than the comparand currency object, otherwise False
         """
         try:
-            if(comparand.getName()!=self.getName()): raise ValueError
+            if comparand.getName()!= self.getName():
+                raise ValueError
             if comparand.noteValue > self.noteValue: return False
             if comparand.noteValue == self.noteValue:
                 if comparand.coinValue > self.coinValue: return False
