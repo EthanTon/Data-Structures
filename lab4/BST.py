e80c87e07f006184e749fc4d02365f3cdd87548d
alexander-cho <44910451+alexander-cho@users.noreply.github.com>
2023-11-18 01:03:36 -0800

    4 traversals

--- lab4/BST.py
+++ lab4/BST.py
@@ -15,51 +15,7 @@ class BST():
             if currNode.data.isEqual(node.data): return True
             elif currNode.data.isGreater(node.data): currNode = currNode.left
             else: currNode = currNode.right
-        return False
-        
-    def inorderTraversal(self, root):
-        result = [] #initialize a result list
-        if root: #if root value in BST exists
-            result = self.inorderTraversal(root.left) #traverse left subtree
-            result.append(root.data) #append current node's data
-            result = result + self.inorderTraversal(root.right) #traverse right subtree
-        return result #return the list
-    
-    def preorderTraversal(self, root):
-        result = [] 
-        if root:
-            result.append(root.data)
-            result = result + self.preorderTraversal(root.left)
-            result = result + self.preorderTraversal(root.right)
-        return result
-
-    def postorderTraversal(self, root):
-        result = []
-        if root:
-            result = result + self.postorderTraversal(root.left)
-            result = result + self.postorderTraversal(root.right)
-            result.append(root.data)
-        return result
-    
-    def levelorderTraversal(self, root):
-        result = []
-        if self.root is None: #check if the BST is empty
-            return result #return an empty list
-
-        queue = Queue() #create a queue
-        queue.enqueue(self.root) #enqueue the root node of the BST
-
-        while not queue.isEmpty(): #loop until the queue is empty
-            node = queue.dequeue() #dequeue the node
-            result.append(node.data) #add that node's data to the result list
-
-            if node.left is not None: #enqueue the left child if it exists
-                queue.enqueue(node.left)
-            if node.right is not None: #enqueue the right child if it exists
-                queue.enqueue(node.right)
-
-        return result
-        
+        return False    
     
     def insert(self, node):
         root = self.root
@@ -101,6 +57,49 @@ class BST():
         if  self.root is None:
             return True
         else: return False
+    
+    def inorderTraversal(self, root):
+        result = [] #initialize a result list
+        if root: #if root value in BST exists
+            result = self.inorderTraversal(root.left) #traverse left subtree
+            result.append(root.data) #append current node's data
+            result = result + self.inorderTraversal(root.right) #traverse right subtree
+        return result #return the list
+    
+    def preorderTraversal(self, root):
+        result = [] 
+        if root:
+            result.append(root.data)
+            result = result + self.preorderTraversal(root.left)
+            result = result + self.preorderTraversal(root.right)
+        return result
+
+    def postorderTraversal(self, root):
+        result = []
+        if root:
+            result = result + self.postorderTraversal(root.left)
+            result = result + self.postorderTraversal(root.right)
+            result.append(root.data)
+        return result
+    
+    def levelorderTraversal(self, root):
+        result = []
+        if self.isEmpty(): #check if the BST is empty
+            return result #return an empty list
+
+        queue = Queue() #create a queue
+        queue.enqueue(self.root) #enqueue the root node of the BST
+
+        while not queue.isEmpty(): #loop until the queue is empty
+            node = queue.dequeue() #dequeue the node
+            result.append(node.data) #add that node's data to the result list
+
+            if node.left is not None: #enqueue the left child if it exists
+                queue.enqueue(node.left)
+            if node.right is not None: #enqueue the right child if it exists
+                queue.enqueue(node.right)
+
+        return result
 
     def empty(self):
         self.root = None
