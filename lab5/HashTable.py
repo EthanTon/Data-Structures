e87927ded9c4385d995cd3fdf91291bdf6a6a54d
Ethan Ton <ethan.m.ton@gmail.com>
2023-11-27 17:24:55 -0800

    Update HashTable.py

--- lab5/HashTable.py
+++ lab5/HashTable.py
@@ -6,10 +6,14 @@ class HashTable:
             self.size = size
             self.map = [None] * size
             self.collisions = 0
+            self.length = 0
         else: raise ValueError
 
     def getLoadFactor(self) -> float:
         return float(self.size/self.collisions)
+    
+    def getCollisions(self) -> int:
+        return self.collisions
 
     def HashingFunction(self,item=None) -> int:
         if item is not None:
@@ -19,23 +23,41 @@ class HashTable:
         else: raise ValueError
 
     def HashInsert(self, item):
-        if self.HashSearch(item) is None:
-            index = self.HashingFunction(item)
-            if map[index] is not None:
-                map[index] = item
-            else: pass
+        index = self.HashingFunction(item)
+        originalIndex = index
+        i = 1  # Quadratic probing increment
 
-    def HashRemove(self,item):
-        index = self.HashSearch(item)
-        if index is not None:
-            map[index] = None
+        while self.map[index] is not None:
+            index = (originalIndex + i**2) % self.size
+            i += 1
+            self.collisions += 1
+            if index == originalIndex:
+                raise ValueError("HashTable is full")
+
+        self.map[index] = item
+        self.length += 1
+
+    def HashRemove(self, item):
+        index = self.HashingFunction(item)
+        originalIndex = index
+
+        while self.map[index] is not None:
+            if item.isEqual(self.map[index]):
+                self.map[index] = None
+                return
+            index = (index + 1) % self.size
+            if index == originalIndex:
+                break  # Item not found
 
     def HashSearch(self, item):
         index = self.HashingFunction(item)
+        originalIndex = index
 
-        if item.isEqual(self.map[index]):
-            return index
-        else: return None
-    
-    def getCollisions(self):
-        return self.collisions
+        while self.map[index] is not None:
+            if item.isEqual(self.map[index]):
+                return index
+            index = (index + 1) % self.size
+            if index == originalIndex:
+                break  # Item not found
+
+        return None
